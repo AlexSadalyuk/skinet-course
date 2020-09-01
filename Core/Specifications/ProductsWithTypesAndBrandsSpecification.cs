429caf923a16af2c24ff5748907a4bc8cb76fa40
Oleksandr Sadaliuk <sadalyukalex@gmail.com>
2020-09-01 17:55:40 +0300

    Update specifications
    Updated all specifications to support pagination, search and filter options

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -4,10 +4,33 @@ namespace Core.Specifications
 {
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
-        public ProductsWithTypesAndBrandsSpecification()
+        public ProductsWithTypesAndBrandsSpecification(ProductSpecParams specParams)
+            :base(x =>
+                (string.IsNullOrEmpty(specParams.Search) || x.Name.ToLower().Contains(specParams.Search)) &&
+                (!specParams.BrandId.HasValue || x.ProductBrandId == specParams.BrandId) &&
+                (!specParams.TypeId.HasValue || x.ProductTypeId == specParams.TypeId)
+            )
         {
             AddInclude(p=>p.ProductBrand);
             AddInclude(p=>p.ProductType);
+            AddOrderBy(p=>p.Name);
+            ApplyPaging(specParams.PageSize * (specParams.PageIndex - 1), specParams.PageSize);
+
+            if(!string.IsNullOrEmpty(specParams.Sort)) 
+            {
+                switch(specParams.Sort) 
+                {
+                    case "priceAsc":
+                        AddOrderBy(p=>p.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderDescendingBy(p=>p.Price);
+                        break;  
+                    default:
+                        AddOrderBy(p=>p.Name);
+                        break;
+                }
+            }
         }
 
         public ProductsWithTypesAndBrandsSpecification(int id)
