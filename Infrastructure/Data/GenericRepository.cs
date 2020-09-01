2fc3a7acaf6d03b94465d3fab182b8bbc04a5dda
Oleksandr Sadaliuk <sadalyukalex@gmail.com>
2020-09-01 17:57:49 +0300

    Update generic repository to be able to count values

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -29,9 +29,10 @@ namespace Infrastructure.Data
         public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec) 
             => await ApplySpecification(spec).ToListAsync();
 
+        public async Task<int> CountAsync(ISpecification<T> spec) 
+            => await ApplySpecification(spec).CountAsync();
+
         private IQueryable<T> ApplySpecification(ISpecification<T> spec) 
-        {
-            return SpecificationEvaluator<T>.GetQueryable(_context.Set<T>().AsQueryable(), spec);
-        }
+            => SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
     }
 }
